eb27b280257228bde07d6cd2a9caf656a42fe9c4
st0unn <quatro0osSs@gmail.com>
2024-11-06 16:53:00 +0200

    edit hw7.2

--- hw7.1.js
+++ hw7.1.js
@@ -2,16 +2,23 @@ let arr = [
     {
         userName:"Test",
         lastName:"Test",
-        <email:"test.test@gmail.com>"
+        email:"test.test@gmail.com"
     },
     {
         userName:"Dmitro",
         lastName:"Porohov",
-        <email:"dmitro.porohov@yahoo.com>"
+        email:"dmitro.porohov@yahoo.com"
     },
     {
         userName:"Andrii",
         lastName:"",
-        <email:"andrii@mail.ru>" // Нам такі не підходять
+        email:"andrii@mail.ru" // Нам такі не підходять
     },
 ];
+
+
+let validEmails = arr
+    .map(item => item.email)
+    .filter(email => /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@(gmail|yahoo)\.com$/.test(email));
+
+console.log(validEmails);
